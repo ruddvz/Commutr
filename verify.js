37b4b58c34b05b19b94ac2b8f6b4ea83c6b99e02
COMMUTR Dev <commutr@dev.local>
2026-03-21 16:26:24 -0400

    chore: remove dev utility scripts

--- verify.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const h = fs.readFileSync('index.html', 'utf8');
-console.log('phone wrapper:',  h.includes('<div class="phone">'));
-console.log('di present:',     h.includes('<div class="di">'));
-console.log('desktnav:',       h.includes('class="desktnav"'));
-console.log('dn-links:',       h.includes('dn-links'));
-console.log('end phone gone:', !h.includes('end phone'));
-console.log('\nFirst 900 chars of body:\n', h.substring(h.indexOf('<body>'), h.indexOf('<body>') + 900));
